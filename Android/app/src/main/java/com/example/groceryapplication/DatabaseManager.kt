e06732b44dd7449190c35271661f0e59f9278408
Andy Huynh <2831414+andy4thehuynh@users.noreply.github.com>
2026-06-06 15:38:56 -0600

    Sorts by name for inventory like iOS and web

--- Android/app/src/main/java/com/example/groceryapplication/DatabaseManager.kt
+++ Android/app/src/main/java/com/example/groceryapplication/DatabaseManager.kt
@@ -213,6 +213,7 @@ class DatabaseManager(private val context: Context) {
             val query = QueryBuilder
                 .select(SelectResult.all())
                 .from(DataSource.collection(collection))
+                .orderBy(Ordering.expression(Expression.property("name")).ascending())
             
             Log.d("DatabaseManager", "🔄 [Reactive API] Setting up query change listener...")
             
@@ -276,6 +277,7 @@ class DatabaseManager(private val context: Context) {
                 val query = QueryBuilder
                     .select(SelectResult.all())
                     .from(DataSource.collection(collection))
+                    .orderBy(Ordering.expression(Expression.property("name")).ascending())
                 
                 val results = query.execute()
                 val groceryItems = mutableListOf<GroceryItem>()
@@ -356,6 +358,7 @@ class DatabaseManager(private val context: Context) {
                 val query = QueryBuilder
                     .select(SelectResult.all())
                     .from(DataSource.collection(collection))
+                    .orderBy(Ordering.expression(Expression.property("name")).ascending())
                 
                 val results = query.execute()
                 val groceryItems = mutableListOf<GroceryItem>()
